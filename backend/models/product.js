646ef8fe7538f6865602c30dc42665cc596824fb
futurelinker <hola@futurelink.me>
2021-04-24 01:44:18 -0500

    Database Connection / Product model created

---
+++ backend/models/product.js
@@ -0,0 +1,28 @@
+const moongose = require('mongoose')
+
+const productSchema = new moongose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+})
+
+const Product = moongose.model("product", productSchema);
+
+module.exports = Product;
