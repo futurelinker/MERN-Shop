1a2acddf208d924dbe5aabf76eb5470e08ba0fb5
futurelinker <hola@futurelink.me>
2021-04-27 23:57:48 -0500

    Store, Reducers and Actions added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./index.css";
 
+// Connects app with redux store
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider
+    //  app acces to the store
+    store={store}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
---
+++ frontend/src/redux/store.js
@@ -0,0 +1,29 @@
+// 1. import dependencies
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+import { cartReducer } from "./reducers/cartReducers";
+import {
+  getProductsReducer,
+  getProductDetailsReducer,
+} from "./reducers/productReducers";
+
+// 2. create reducer
+const reducer = combineReducers({
+  cart: cartReducer,
+  getProducts: getProductsReducer,
+  getProductDetails: getProductDetailsReducer,
+});
+// thunk allows to make async request on actions
+const middleware = [thunk];
+
+// 3. Create store
+const store = createStore(
+  // 4. reducer
+  reducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+// 5. export store and connect to index.js
+export default store;
